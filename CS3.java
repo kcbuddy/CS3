d59b04cbdf780125a29f37a76364d34fd955862b
kcbuddy <34259600+kcbuddy@users.noreply.github.com>
2018-03-14 21:33:27 -0400

    Create CS3.java
    This will be the main file

---
+++ CS3.java
@@ -0,0 +1,26 @@
+//MAIN FILE
+package cs3;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+public class CS3 {
+    NetworkInterface networkInterface;
+    ObjectInputStream input;
+    ServerSocket serverSocket;
+    Socket socket;
+    Inet4Address ipv4Address;
+    Inet6Address ipv6Address;
+    NetPermission netPermission;
+    SocketPermission socketPermission;
+    ArrayList badConnList;
+    ArrayList ipv4List;
+    ArrayList bannedConList;
+    BadConn bandConn;
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
